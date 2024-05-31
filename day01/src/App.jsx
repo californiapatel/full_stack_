c5aa25b643c04d49637a8d90c38825aeae34110f
aastha sapovadia <californiapatel26@gmail.com>
2024-05-31 14:46:00 +0530

    day01

---
+++ day01/src/App.jsx
@@ -0,0 +1,94 @@
+import logo from './logo.svg';
+import './App.css';
+import './style.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <div className="logo1">
+          <center><h1>Still</h1></center>
+        </div>
+        <nav>
+          <p>THE SPACE</p>
+          <p>AMENITIES</p>
+          <p>OUR STORY</p>
+          <p>BOOK NOW</p>
+        </nav>
+      </header>
+      <section>
+        <div className="pen">
+          <div className="pen1"><img src="https://images.unsplash.com/photo-1563911302283-d2bc129e7570?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="not found" /></div>
+          <div className="pen2">
+            <h1>
+              A boutique <br />
+              hotel for <br />
+              those seeking <br />
+              stillness.
+            </h1>
+            <button>BOOK A ROOM</button>
+          </div>
+        </div>
+        <div className="pencil">
+          <div className="pencil1">
+            <h1>
+              Our Space was designed to be <br />
+              relaxing and comforting. Each <br />
+              detail has been thoughtfully <br />
+              considered in order to make <br />
+              out guests feel at home. the <br />
+              layout an decor of our rooms differ <br />
+              but all share the same coty and <br />
+              restorable vibes.
+            </h1>
+            <button>Explore Rooms</button>
+          </div>
+          <div className="pencil2">
+            <img src="https://images.unsplash.com/photo-1549294413-26f195200c16?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="not found" />
+          </div>
+        </div>
+        <div className="eraser">
+          <div className="eraser1">
+            <img src="https://images.unsplash.com/photo-1535827841776-24afc1e255ac?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="not found" />
+          </div>
+          <div className="eraser2">
+            <h1>
+              our space was designed to be <br />
+              relaxing and comforting. Each detail <br />
+              has been thoughtfully considered in <br />
+              order to make our guests feel at <br />
+              home. the layout and decor of our <br />
+              room differ but all share the same <br />
+              coty and restorable vibes.
+            </h1>
+            <button>Views Amenities</button>
+          </div>
+        </div>
+        <div className="shaper">
+          <div className="shaper1">
+            <h1>
+            The luxurious hotel offers stunning ocean <br />
+            views, elegant rooms, and exceptional <br />
+            service. Guests enjoy a rooftop pool, <br />
+            gourmet dining, and a serene spa. <br />
+            Located in the heart of the city, <br />
+            it's perfect for both business <br />
+            and leisure travelers.
+            </h1>
+            <button>Exlpore More</button>
+          </div>
+          <div className="shaper2">
+            <img src="https://images.unsplash.com/photo-1506812428898-fe5a708f5359?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="not found" />
+            </div>
+        </div>
+        <h1 id='one-1'>
+          <center>
+            Created By : Aastha Sapovadia
+          </center>
+        </h1>
+      </section>
+    </div>
+  );
+}
+
+export default App;
